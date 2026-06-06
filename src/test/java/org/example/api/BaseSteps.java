ee3a9af1ec67e17a3e1b65cb5a63195689d7f511
Kechek Karine <kechek.karine@gmail.com>
2026-06-06 20:28:43 +0300

    add base steps

--- src/test/java/org/example/api/BaseSteps.java
+++ src/test/java/org/example/api/BaseSteps.java
@@ -1,4 +1,12 @@
 package org.example.api;
 
+import io.restassured.response.Response;
+
 public class BaseSteps {
+    public static void checkStatusCode(Response response, int statusCode) {
+        response.then().statusCode(statusCode);
+    }
+    public static Object getValue(Response response, String name) {
+        return response.then().extract().path(name);
+    }
 }
