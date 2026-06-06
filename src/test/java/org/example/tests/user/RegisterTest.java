3ea6340af293c080916c3cea00718d233f8984e0
Kechek Karine <kechek.karine@gmail.com>
2026-06-06 20:30:52 +0300

    add RegisterTest

--- src/test/java/org/example/tests/user/RegisterTest.java
+++ src/test/java/org/example/tests/user/RegisterTest.java
@@ -1,36 +1,134 @@
-package org.example;
+package org.example.tests.user;
 
+import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.example.api.BaseSteps;
 import org.example.data.User;
+import org.example.pages.HomePage;
+import org.example.steps.ActionsUser;
+import org.example.utils.DriverExtension;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.WebDriver;
 
 import java.net.HttpURLConnection;
 
 import static java.net.http.HttpRequest.BodyPublishers.ofString;
 import static org.example.utils.EnvConfig.BASE_URL;
 
+@Slf4j
 public class RegisterTest {
+    @RegisterExtension
+    private final DriverExtension extension = new DriverExtension();
     ActionsUser actionsUser = new ActionsUser();
+
+
     @Test
-    public void createUser() {
+    public void createUser1() {
         RestAssured.baseURI = BASE_URL;
         User user = User.userWithRandomField();
         Response response = actionsUser.createUser(user);
-        checkStatusCode(response, HttpURLConnection.HTTP_CREATED);
-        int id = (int) getValue(response, "user.id");
-        String token = (String) getValue(response, "access_token.access_token");
+        BaseSteps.checkStatusCode(response, HttpURLConnection.HTTP_CREATED);
+        int id = (int) BaseSteps.getValue(response, "user.id");
+        String token = (String) BaseSteps.getValue(response, "access_token.access_token");
         Response response2 = actionsUser.loginUser(user);
-        String token2 = (String) getValue(response2, "token.access_token");
+        String token2 = (String) BaseSteps.getValue(response2, "token.access_token");
         System.out.println(token);
         System.out.println(token2);
         //actionsUser.deleteUser(token, id);
     }
-    public void checkStatusCode(Response response, int statusCode) {
-        response.then().statusCode(statusCode);
-    }
-    public Object getValue(Response response, String name) {
-        return response.then().extract().path(name);
+
+    @Test
+    @DisplayName("Successful registration")
+    @Description("Successful registration")
+    public void createUser() throws InterruptedException {
+        WebDriver driver = extension.getDriver();
+        User user = User.userWithRandomField();
+
+        var homePage = new HomePage(driver);
+
+        homePage.openPage();
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterUserData(user);
+        var homeAuthPage = homePage.clickCreateAccountButton();
+        homeAuthPage.checkContent();
     }
 
+    @Test
+    @DisplayName("Registration exist user")
+    @Description("Registration exist user")
+    public void createExistUser() throws InterruptedException {
+        WebDriver driver = extension.getDriver();
+        User user = User.userWithRandomField();
+
+        var homePage = new HomePage(driver);
+
+        homePage.openPage();
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterUserData(user);
+        var homeAuthPage = homePage.clickCreateAccountButton();
+        homeAuthPage.logoutUser();
+
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterUserData(user);
+        homePage.clickCreateAccountButton();
+        homePage.checkError();
+    }
+
+    @Test
+    @DisplayName("Registration without password")
+    @Description("Registration without password")
+    public void createUserWithoutPassword() throws InterruptedException {
+        WebDriver driver = extension.getDriver();
+        User user = User.userWithRandomField();
+
+        var homePage = new HomePage(driver);
+
+        homePage.openPage();
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterEmail(user);
+        var homeAuthPage = homePage.clickCreateAccountButton();
+        homeAuthPage.checkContent();
+    }
+
+    @Test
+    @DisplayName("Registration without repeat password")
+    @Description("Registration without repeat password")
+    public void createUserWithoutRepeatPassword() throws InterruptedException {
+        WebDriver driver = extension.getDriver();
+        User user = User.userWithRandomField();
+
+        var homePage = new HomePage(driver);
+
+        homePage.openPage();
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterEmailAndPassword(user);
+        homePage.clickCreateAccountButton();
+        homePage.checkPasswordsDontMatch();
+    }
+
+    @Test
+    @DisplayName("Registration with wrong repeat password")
+    @Description("Registration with wrong repeat password")
+    public void createUserWithWrongRepeatPassword() throws InterruptedException {
+        WebDriver driver = extension.getDriver();
+        User user = User.userWithRandomField();
+
+        var homePage = new HomePage(driver);
+
+        homePage.openPage();
+        homePage.clickAccountButton();
+        homePage.clickNoAccount();
+        homePage.enterWrongRepeatPassword(user);
+        homePage.clickCreateAccountButton();
+        homePage.checkPasswordsDontMatch();
+    }
 }
