1605f28177668d545ddbf015af52f8db5c9f44c8
Kechek Karine <kechek.karine@gmail.com>
2026-06-06 20:26:31 +0300

    add brousers

--- src/test/java/org/example/utils/EnvConfig.java
+++ src/test/java/org/example/utils/EnvConfig.java
@@ -1,5 +1,10 @@
 package org.example.utils;
 
+import java.time.Duration;
+
 public class EnvConfig {
     public static final String BASE_URL = "https://qa-desk.education-services.ru/";
+    public static final String PATH_TO_YANDEX_BROWSER = "C:\\Program Files\\Yandex\\YandexBrowser\\Application\\browser.exe";
+    public static final String DRIVER_VERSION = "146.0.7680.165";
+    public static final Duration EXPLICIT_TIMEOUT = Duration.ofSeconds(10);
 }
