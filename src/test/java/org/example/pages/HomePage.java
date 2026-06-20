e4b066aec571f68cc7c392f85983c1288e824918
Kechek Karine <kechek.karine@gmail.com>
2026-06-20 19:34:16 +0300

    modify HomePage

--- src/test/java/org/example/pages/HomePage.java
+++ src/test/java/org/example/pages/HomePage.java
@@ -1,22 +1,32 @@
 package org.example.pages;
 
 import io.qameta.allure.Step;
+import org.example.data.Ad;
 import org.example.data.User;
+import org.example.pages.ad.AdPage;
+import org.example.pages.ad.CreateListingPage;
 import org.example.pages.user.HomeAuthPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.example.utils.EnvConfig.BASE_URL;
 
 public class HomePage extends BasePage {
     protected final By accountButton = By.xpath("//button[text()='Вход и регистрация']");
     protected final By noAccountButton = By.xpath("//button[text()='Нет аккаунта']");
+    protected final By enterButton = By.xpath("//button[text()='Войти']");
     protected final By inputEmail = By.cssSelector("[placeholder='Введите Email']");
     protected final By inputPassword = By.cssSelector("[placeholder='Пароль']");
     protected final By inputRepeatPassword = By.cssSelector("[placeholder='Повторите пароль']");
     protected final By createAccountButton = By.xpath("//button[text()='Создать аккаунт']");
     protected final By error = By.xpath("//span[@class='input_span__yWPqB' and text()='Ошибка']");
     protected final By dontMatch = By.xpath("//span[@class='input_span__yWPqB' and text()='Пароли не совпадают']");
+    protected final By errorLogin = By.xpath("//span[@class='input_span__yWPqB' and text()='Логин или пароль неверны']");
+    protected final By errorCreate = By.xpath("//h1[text()='Чтобы разместить объявление, авторизуйтесь']");
+    protected final By adButton = By.xpath("//button[text()='Разместить объявление']");
+
+    WebElement card;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -24,6 +34,15 @@ public class HomePage extends BasePage {
 
     public void openPage() {
         driver.get(BASE_URL);
+    }
+
+    public AdPage openAdPage(Ad ad, int adId) {
+        //driver.get(BASE_URL + "/listing/" + adId);
+
+        searchAd(ad);
+        card.click();
+
+        return new AdPage(driver);
     }
 
     @Step("Click on the button (login and registration)")
@@ -36,6 +55,14 @@ public class HomePage extends BasePage {
     public void clickNoAccount() {
         waitLocator(noAccountButton);
         waitClickable(noAccountButton).click();
+    }
+
+    @Step("Click on the button (enter)")
+    public HomeAuthPage clickEnter() {
+        waitLocator(enterButton);
+        waitClickable(enterButton).click();
+
+        return new HomeAuthPage(driver);
     }
 
     @Step("Input user's fields")
@@ -56,6 +83,12 @@ public class HomePage extends BasePage {
         enterNewValue(inputPassword, user.getPassword());
     }
 
+    @Step("Input user's email and wrong password")
+    public void enterEmailAndWrongPassword(User user) {
+        enterNewValue(inputEmail, user.getEmail());
+        enterNewValue(inputPassword, "wrongPassword");
+    }
+
     @Step("Input wrong repeat password")
     public void enterWrongRepeatPassword(User user) {
         enterNewValue(inputEmail, user.getEmail());
@@ -64,18 +97,76 @@ public class HomePage extends BasePage {
     }
 
     @Step("Click on the button (create account)")
-    public HomeAuthPage clickCreateAccountButton() throws InterruptedException {
+    public HomeAuthPage clickCreateAccountButton() {
         waitLocator(createAccountButton);
         waitClickable(createAccountButton).click();
 
         return new HomeAuthPage(driver);
     }
 
-    public void checkError() {
+
+    @Step("Click on the button (create account) and stay homePage")
+    public void clickCreateAccountButtonAndStay() {
+        waitLocator(createAccountButton);
+        waitClickable(createAccountButton).click();
+    }
+
+    public void checkRegisterError() {
         waitLocator(error);
     }
 
-    public void checkPasswordsDontMatch() {
+    public void checkCreateError() {
+        waitLocator(errorCreate);
+    }
+
+    public void checkLoginError() {
+        waitLocator(errorLogin);
+    }
+
+    public void checkPasswordsAreDifferent() {
         waitLocator(dontMatch);
+    }
+
+
+    @Step("Click on the button (place an ad) without login")
+    public void clickCreateAdButtonHomePage() {
+        waitLocator(adButton);
+        WebElement el = waitClickable(adButton);
+        el.click();
+    }
+
+    @Step("Search the ad")
+    public void searchAd(Ad ad) {
+        By cardLocator = getCardLocator(ad, false);
+
+        int attempts = 1;
+        int maxAttempts = getMaxAttempts(bar);
+
+        while (attempts <= maxAttempts) {
+            if (!driver.findElements(cardLocator).isEmpty()) {
+                card = waitClickable(cardLocator);
+                checkCost(card, ad);
+
+                return;
+            }
+            presenceLocator(bar);
+            scrollToLocator(bar);
+            if (driver.findElements(arrowButton).isEmpty()) {
+                throw new AssertionError("Кнопка переключения не найдена");
+            }
+
+            WebElement next = presenceLocator(arrowButton);
+            String oldPage = presenceLocator(bar).getText();
+            String oldAd = presenceLocator(adsList).getText();
+
+            if (next.isEnabled()) {
+                next.click();
+            }
+            reloadPage(bar, oldPage);
+            reloadPage(adsList, oldAd);
+
+            attempts++;
+        }
+        throw new AssertionError("Объявление '" + ad.getName() + "' отсутствует");
     }
 }
