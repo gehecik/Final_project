5db48523d8ecec0863e440e4e246fe0c6bed6ad9
Kechek Karine <kechek.karine@gmail.com>
2026-06-20 19:47:16 +0300

    add AdPage

--- src/test/java/org/example/pages/ad/AdPage.java
+++ src/test/java/org/example/pages/ad/AdPage.java
@@ -1,4 +1,37 @@
 package org.example.pages.ad;
 
-public class AdPage {
+import org.example.data.Ad;
+import org.example.pages.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AdPage extends BasePage {
+    protected final By categoryCondition = By.className("listing_categoryColor__Zj1m4");
+    protected final By name = By.className("h1Mobile");
+    protected final By description = By.xpath("//p[@class='spanGlobal']");
+    protected final By city = By.xpath("//h3[@class='subtitle']");
+
+
+    public AdPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void checkNewAdWithoutImg(Ad ad) {
+        checker(categoryCondition, ad.getCategory());
+        checker(categoryCondition, ad.getCondition());
+        checker(name, ad.getName());
+        checker(description, ad.getDescription());
+        checker(city, ad.getCity());
+
+    }
+
+    public void checker(By locator, String subs) {
+        WebElement el = presenceLocator(locator);
+        String text = el.getText();
+        assertTrue(text.contains(subs));
+    }
+
 }
