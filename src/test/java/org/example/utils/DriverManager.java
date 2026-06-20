ff70b319b47a01f727726e42dc9731bc95dcdec9
Kechek Karine <kechek.karine@gmail.com>
2026-06-20 19:39:55 +0300

    DriverManager

--- src/test/java/org/example/utils/DriverManager.java
+++ src/test/java/org/example/utils/DriverManager.java
@@ -1,4 +1,15 @@
 package org.example.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverManager {
+    private static WebDriver driver;
+
+    public static void setDriver(WebDriver webDriver) {
+        driver = webDriver;
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
 }
